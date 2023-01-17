5858d34cf3dccde6437fdc6f46d767ead50ad2df
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2023-01-17 17:29:17 +0100

    simen skrev litt kode hjemme

--- kode/javascript/spill.js
+++ kode/javascript/spill.js
@@ -1,8 +1,9 @@
 
+const word = "SiMen".toLowerCase(); 
 
 const boxesDivEl = document.querySelector("#boxes");
 
-const dimentions = [5, 6]
+const dimentions = [word.length, 6];
 
 for (let i=1; i <= dimentions[0]; i++) {
     let box = document.createElement("div");
@@ -21,19 +22,88 @@ for (let i=1; i <= dimentions[0]; i++) {
 
 const boxElList = document.querySelectorAll(".BOX");
 const boxElObj = {};
-
 boxElList.forEach(boxEl => {
     Object.assign(boxElObj, {[String(boxEl.id)]: boxEl});
 });
+/* console.log(boxElObj); */
 
-console.log(boxElObj);
 
 const charNum = Array.from(Array(26)).map((e, i) => i + 65);
 const bigChar = charNum.map((x) => String.fromCharCode(x));
 const alphabet = [];
+/* console.log(alphabet); */
+
 const guess = [];
+/* ["s", "i", "m", "e", "n"] */
+
 const oldGuesses = [];
+/* [["s", 10], ["i", 5], ["m", 0], ["e", 0], ["n", 10]] */
+
 var guesses = 1;
+/* increments with each guess */
+
+function checkGuess(checkingArr, correctStr) {
+
+    let word = checkingArr.join("");
+    const checkArr = [];
+
+    /* 
+    0  = OK
+    5  = ALMOST
+    10 = WRONG
+    */
+
+    if (word.length !== correctStr.length) {
+        return "Not the same length.";
+    }
+
+    for (let i=0; i < word.length; i++) {
+        
+        if (word[i] === correctStr[i]) {
+            checkArr.push(0);
+            continue;
+        }
+
+        let foundOne = false;
+
+        /* NOTE: fint å legge til slik at den sjekker mot ordliste så man ikke kan skrive hva man vil! */
+
+        for (let j=0; j < correctStr.length; j++) {
+            if (j === i) {continue;}
+
+            if (word[i] === correctStr[j]) {
+                checkArr.push(5);
+                foundOne = true;
+            }
+        }
+
+        if (!foundOne) {
+            checkArr.push(10);
+        }
+
+    }
+
+    return checkArr;
+
+}
+
+
+
+function saveGuess() {
+    let result = checkGuess(guess, word);
+
+    const letterAndResult = []
+
+    guess.forEach((letter, index) => {
+        letterAndResult.push([letter, result[index]])
+    });
+
+    oldGuesses.push(letterAndResult);
+    guess.length = 0;
+    guesses++;
+    updateBoxes();
+}
+
 
 function clearBoxes() {
     boxElList.forEach(element => {
@@ -47,18 +117,31 @@ function updateBoxes() {
     clearBoxes();
 
     if (guesses > 1) {
-        oldGuesses.forEach(oldGuess => {
-            for (let j = 1; j <= oldGuess.length; j++) {
-                boxElObj["COL" + String() + "_ROW" + String(guesses)].innerHTML = guess[i - 1];
+        for (let i = 1; i <= oldGuesses.length; i++) {
+            for (let j = 1; j <= oldGuesses[i - 1].length; j++) {
+                boxElObj["COL" + String(j) + "_ROW" + String(i)].innerHTML = oldGuesses[i - 1][j - 1][0].toUpperCase();
+                switch (oldGuesses[i - 1][j - 1][1]) {
+                    case 0: {
+                        if (!(boxElObj["COL" + String(j) + "_ROW" + String(i)].className.includes("correct"))) {boxElObj["COL" + String(j) + "_ROW" + String(i)].className += " correct";}
+                        break;
+                    }
 
-                // dette funker ikke du må fikse senere simen
+                    case 5: {
+                        if (!(boxElObj["COL" + String(j) + "_ROW" + String(i)].className.includes("almost"))) {boxElObj["COL" + String(j) + "_ROW" + String(i)].className += " almost";}
+                        break;
+                    }
 
+                    case 10: {
+                        if (!(boxElObj["COL" + String(j) + "_ROW" + String(i)].className.includes("wrong"))) {boxElObj["COL" + String(j) + "_ROW" + String(i)].className += " wrong";}
+                        break;
+                    }
+                }
             }
-        });
+        }
     }
 
     for (let i=1; i <= guess.length; i++) {
-        boxElObj["COL" + String(i) + "_ROW" + String(guesses)].innerHTML = guess[i - 1];
+        boxElObj["COL" + String(i) + "_ROW" + String(guesses)].innerHTML = guess[i - 1].toUpperCase();
     }
 
 }
@@ -73,6 +156,13 @@ bigChar.forEach(element => {
 
 document.addEventListener("keydown", e => {
 
+    if (e.key === "Enter") {
+        if (guess.length !== 5) {
+            return;
+        };
+
+        saveGuess();
+    };
 
     if (e.key === ("Backspace" || "Delete")) {
         guess.pop();
